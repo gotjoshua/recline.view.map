66605d128af1dd37b571d55dea41465ada083a4c
gotjosh <github@begreen.nu>
2015-08-26 11:05:39 -0400

    adding stamen watercolor tiles for outer zoom

--- 1.0b1/view.map.js
+++ 1.0b1/view.map.js
@@ -460,12 +460,21 @@ my.Map = Backbone.View.extend({
   _setupMap: function(){
     var self = this;
     this.map = new L.Map(this.$map.get(0));
-
+	var Stamen_Watercolor = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png', {
+		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+		subdomains: 'abcd',
+		minZoom: 1,
+		maxZoom: 16,
+		ext: 'png'
+	});
+	
     var mapUrl = "http://otile{s}-s.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png";
     var osmAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">';
-    var bg = new L.TileLayer(mapUrl, {maxZoom: 18, attribution: osmAttribution ,subdomains: '1234'});
-    this.map.addLayer(bg);
-
+    var bg = new L.TileLayer(mapUrl, {minZoom: 16, maxZoom: 18, attribution: osmAttribution ,subdomains: '1234'});
+    
+	this.map.addLayer(bg);
+	this.map.addLayer(Stamen_Watercolor);
+	
     this.markers = new L.MarkerClusterGroup(this._clusterOptions);
 
     // rebind this (as needed in e.g. default case above)
